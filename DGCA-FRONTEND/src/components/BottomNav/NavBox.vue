1545d8c858b7ec931ef689bce5e15c54efd74356
ABDUL AZIZ <abaz.my.id@gmail.com>
2023-04-19 17:25:29 +0700

    add watermark

--- DGCA-FRONTEND/src/components/BottomNav/NavBox.vue
+++ DGCA-FRONTEND/src/components/BottomNav/NavBox.vue
@@ -3,9 +3,12 @@ import { RouterLink } from 'vue-router'
 </script>
 <template>
   <div
-    class="fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600"
+    class="fixed bottom-0 left-0 z-50 w-full h-[85px] bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600"
   >
-    <div class="grid h-full max-w-lg grid-cols-2 mx-auto font-medium">
+    <p class="text-center text-sm border-b opacity-70 text-slate-600">
+      Created by: Indah Khairunnisa
+    </p>
+    <div class="grid h-[90%] max-w-lg grid-cols-2 mx-auto font-medium">
       <RouterLink
         to="/"
         class="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
